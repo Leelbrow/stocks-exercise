4d3530b38809f58e6fbd7113f0c91eb36bb2fa80
Leelbrow <veocsi@gmail.com>
2023-05-29 03:31:39 +0200

    Fix favorite handling

--- src/_shared/components/stock-card/stock-card.tsx
+++ src/_shared/components/stock-card/stock-card.tsx
@@ -1,11 +1,14 @@
 import Link from "next/link";
 import { FC, useCallback, useMemo } from "react";
 import { toast } from "react-hot-toast";
+import { AiFillStar, AiOutlineStar } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { selectFavorites } from "../../../favorites/model/favorites.selectors";
-import { addFavorite } from "../../../favorites/model/favorites.slice";
+import {
+  addFavorite,
+  removeFavorite,
+} from "../../../favorites/model/favorites.slice";
 import styles from "./stock-card.module.scss";
-import { AiOutlineStar, AiFillStar } from "react-icons/ai";
 
 type StockCardProps = {
   readonly symbol: string;
@@ -21,9 +24,14 @@ const StockCard: FC<StockCardProps> = ({ symbol, name }) => {
   }, [favorites, symbol]);
 
   const handleFavoritesButtonClick = useCallback(() => {
-    dispatch(addFavorite({ symbol, name }));
-    toast.success("Favorite added!");
-  }, [dispatch, symbol, name]);
+    if (!isFavorite) {
+      dispatch(addFavorite({ symbol, name }));
+      toast.success("Favorite added!");
+    } else {
+      dispatch(removeFavorite(symbol));
+      toast.success("Favorite removed!");
+    }
+  }, [dispatch, isFavorite, symbol, name]);
 
   return (
     <div className={styles.container}>
@@ -37,7 +45,11 @@ const StockCard: FC<StockCardProps> = ({ symbol, name }) => {
         className={styles.favoritesButton}
         onClick={handleFavoritesButtonClick}
       >
-        <AiOutlineStar className={styles.favoritesIcon} />
+        {!isFavorite ? (
+          <AiOutlineStar className={styles.favoritesIcon} />
+        ) : (
+          <AiFillStar className={styles.favoritesIcon} />
+        )}
       </button>
     </div>
   );
